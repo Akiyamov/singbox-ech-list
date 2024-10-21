a6414422ed46dc9c40da6dec58ab65a3519c7e32
Baklushin <baklushin@itpc.ru>
2024-10-21 15:51:04 +0500

    Fix script for sing-box

--- get_ech.sh
+++ get_ech.sh
@@ -17,7 +17,7 @@ start_time=`date +%s`
 while read domain; do 
   if dig type65 +noall +answer $domain @1.1.1.1 | grep ech >/dev/null
     then echo $domain >> domains_ech.txt && number_of_domains_ech=$((number_of_domains_ech + 1))
-    else echo $(cat domains.json | jq ".rules[0].domain_suffix[0] += [\"$domain\"]") > domains.json && echo $domain >> domains_noech.txt && number_of_domains_noech=$((number_of_domains_noech + 1))
+    else echo $(cat domains.json | jq ".rules[0].domain_suffix |= .  + [\"$domain\"]") > domains.json && echo $domain >> domains_noech.txt && number_of_domains_noech=$((number_of_domains_noech + 1))
   fi
 done <domains_all.lst 
 finish_time=`date +%s`
