a285b6960677bef7ecab71f35628fc183b7f82d5
mirdukkkkk <69540930+mirdukkkkk@users.noreply.github.com>
2024-11-02 09:49:08 +1300

    Automation of list creation & add parallelism (#1)
    * Create worker.yml
    
    * parallel processing & dependency checking
    
    * removing variables that are not used
    
    * Edit actions
    
    ---------
    
    Co-authored-by: Akiyamov <116005851+Akiyamov@users.noreply.github.com>

--- get_ech.sh
+++ get_ech.sh
@@ -1,33 +1,68 @@
 #/bin/bash
 
-wget -qO domains_all.lst https://github.com/1andrevich/Re-filter-lists/releases/latest/download/domains_all.lst 
-echo '{
-    "version": 1,
-    "rules": [
-        {
-            "domain_suffix": [
-            ]
-        }
-    ]
-}' > domains_ech.json
-echo '{
-    "version": 1,
-    "rules": [
-        {
-            "domain_suffix": [
-            ]
-        }
-    ]
-}' > domains_noech.json
-echo '[]' > amnezia.json
+# Checking dependencies
+if ! command -v jq &> /dev/null; then
+  echo "Package \"jq\" is not installed. Please install \"jq\" to continue"
+  exit 1
+fi
+
+if ! command -v dig &> /dev/null; then
+  echo "Package \"dig\" is not installed. Please install \"dig\" to continue"
+  exit 1
+fi
+
+if ! command -v parallel &> /dev/null; then
+  echo "Package \"parallel\" is not installed. Sequential mode will be used"
+fi
+
+# Getting a file with blocked domains
+wget -qO domains_all.lst https://github.com/1andrevich/Re-filter-lists/releases/latest/download/domains_all.lst
+
+# Initializing files
+> domains_ech.lst
+> domains_noech.lst
+> domains_noech_dnsmasq.lst
+> domains_ech.json
+> domains_noech.json
+> amnezia.json
+
 number_of_domains=$(wc -l < domains_all.lst)
-number_of_domains_noech=0
-number_of_domains_ech=0
 start_time=`date +%s`
-while read domain; do 
-  if dig type65 +noall +answer $domain @1.1.1.1 | grep ech >/dev/null
-    then echo $(cat domains_ech.json | jq ".rules[0].domain_suffix |= .  + [\"$domain\"]") > domains_ech.json && echo $domain >> domains_ech.lst && number_of_domains_ech=$((number_of_domains_ech + 1))
-    else echo $(cat domains_noech.json | jq ".rules[0].domain_suffix |= .  + [\"$domain\"]") > domains_noech.json && echo "nftset=/$domain/4#inet#fw4#vpn_domains" >> domains_noech_dnsmasq.lst && echo $domain >> domains_noech.lst && echo $(cat amnezia.json | jq ". |= . + [{\"hostname\": \"$domain\", \"ip\": \"\"}]") > amnezia.json && number_of_domains_noech=$((number_of_domains_noech + 1))
+
+echo "Number of domains: $number_of_domains"
+
+# Function checking the domain and writing to the corresponding .lst file
+process_domain() {
+  local domain="$1"
+
+  if dig type65 +noall +answer "$domain" @1.1.1.1 | grep -q ech; then
+    echo "$domain" >> domains_ech.lst
+  else
+    echo "$domain" >> domains_noech.lst
+    echo "nftset=/$domain/4#inet#fw4#vpn_domains" >> domains_noech_dnsmasq.lst
   fi
-done <domains_all.lst 
+}
+
+export -f process_domain
+
+# Selecting a processing method depending on the presence of a dependency
+if command -v parallel &> /dev/null; then
+  cat domains_all.lst | parallel -j 4 process_domain
+else
+  while IFS= read -r domain; do
+    process_domain "$domain"
+  done < domains_all.lst
+fi
+
+# Saving .json files
+printf "\rGenerating domains_ech.json file from created list"
+jq -R -s -c '{version: 1, rules: [{ domain_suffix: split("\n") | .[:-1]}]}' domains_ech.lst > domains_ech.json
+printf "\rGenerating domains_noech.json file from created list"
+jq -R -s -c '{version: 1, rules: [{ domain_suffix: split("\n") | .[:-1]}]}' domains_noech.lst > domains_noech.json
+printf "\rGenerating amnezia.json file from created list"
+jq -R -s -c 'split("\n") | .[:-1] | map({hostname: ., ip: ""})' domains_noech.lst > amnezia.json
+
+printf "\rGeneration of .json files has been successfully completed!"
+
 finish_time=`date +%s`
+echo -e "\nCompleted for $((finish_time - start_time)) seconds."
